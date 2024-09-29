8811f262fd3dda999c4d24af738eb2e3350ce3cc
Surya-Kumar-D <suryakumar.dhanasekar@gmail.com>
2024-09-29 19:45:13 +0530

    Binary Search Tree

---
+++ BinarySearchTree.js
@@ -0,0 +1,58 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+  insert(value) {
+    let newNode = new Node(value);
+    if (!this.root) {
+      this.root = newNode;
+      return this;
+    } else {
+      let currentNode = this.root;
+      while (true) {
+        if (value === currentNode.value) return undefined;
+        if (value < currentNode.value) {
+          if (currentNode.left === null) {
+            currentNode.left = newNode;
+            return this;
+          } else {
+            currentNode = currentNode.left;
+          }
+        } else if (value > currentNode.value) {
+          if (currentNode.right === null) {
+            currentNode.right = newNode;
+            return this;
+          } else {
+            currentNode = currentNode.right;
+          }
+        }
+      }
+    }
+  }
+  find(value) {
+    if (!this.root) return false;
+
+    let currentNode = this.root;
+    if (currentNode === value) return currentNode;
+    let found = false;
+    while (currentNode && !found) {
+      if (value < currentNode.value) {
+        currentNode = currentNode.left;
+      } else if (value > currentNode.value) {
+        currentNode = currentNode.right;
+      } else {
+        return true;
+      }
+    }
+
+    return false;
+  }
+}
